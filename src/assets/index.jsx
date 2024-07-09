e8410a192a936c1467b3c04df6d801a33b3ce697
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-07-09 09:15:31 +0700

    create file to export image and video

---
+++ src/assets/index.jsx
@@ -0,0 +1,17 @@
+import arrow_btn from "./arrow_btn.png";
+import image1 from "./image1.png";
+import image2 from "./image2.png";
+import image3 from "./image3.png";
+import pause_icon from "./pause_icon.png";
+import play_icon from "./play_icon.png";
+import video1 from "./video1.mp4";
+
+export const assets = {
+  arrow_btn,
+  image1,
+  image2,
+  image3,
+  pause_icon,
+  play_icon,
+  video1,
+};
